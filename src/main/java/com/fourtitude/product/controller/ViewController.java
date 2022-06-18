94e276b41624f2863a5ff3aa18f930f582a53250
mhafizkn <hafizkn98@gmail.com>
2022-06-19 01:07:01 +0800

    Display data through JSON :construction:

---
+++ src/main/java/com/fourtitude/product/controller/ViewController.java
@@ -0,0 +1,12 @@
+package com.fourtitude.product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+    @RequestMapping("/")
+    public String viewProducts() {
+       return "list-products";
+    }
+}
