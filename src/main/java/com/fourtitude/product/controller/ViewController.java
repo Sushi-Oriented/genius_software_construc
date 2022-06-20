6bbf60f4fed326606711c33bcb0ca3bff606ad16
mhafizkn <hafizkn98@gmail.com>
2022-06-20 17:01:22 +0800

    Cinoi Progression :construction:
    Changes:
    - ProductController.java
    - ViewController.java
    - add-edit-product.html
    - list-products.html

--- src/main/java/com/fourtitude/product/controller/ViewController.java
+++ src/main/java/com/fourtitude/product/controller/ViewController.java
@@ -2,11 +2,22 @@ package com.fourtitude.product.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ViewController {
     @RequestMapping("/")
     public String viewProducts() {
        return "list-products";
+    }
+
+    @RequestMapping("/add-product")
+    public String addProduct() {
+        return "add-edit-product";
+    }
+
+    @RequestMapping(value = "/edit-product", params = "id")
+    public String editProduct() {
+        return "add-edit-product";
     }
 }
