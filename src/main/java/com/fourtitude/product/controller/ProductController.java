efc9aa8f5c30c42d52f6e57c4ff56c28f0065ac3
Alifah Ilyana <alifaahilyanaa@gmail.com>
2022-06-21 13:52:35 +0800

    Remove DataResponseException

--- src/main/java/com/fourtitude/product/controller/ProductController.java
+++ src/main/java/com/fourtitude/product/controller/ProductController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.fourtitude.product.exception.DataResponseException;
 import com.fourtitude.product.exception.RecordNotFoundException;
 import com.fourtitude.product.model.ProductEntity;
 import com.fourtitude.product.service.ProductService;
