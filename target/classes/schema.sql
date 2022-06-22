ca14f86b25b8b3f30fac180186d6500c70fc2a35
Alifah Ilyana <alifaahilyanaa@gmail.com>
2022-06-22 09:35:36 +0800

    Update classes based on resources

--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -1,31 +1,21 @@
 -- PostGres SQL Schema
 
--- DROP TABLE IF EXISTS products;
---
--- CREATE TABLE products (
---                           id serial PRIMARY KEY,
---                           code VARCHAR(9) UNIQUE NOT NULL ,
---                           name VARCHAR(90) NOT NULL,
---                           category VARCHAR(28) NOT NULL,
---                           brand VARCHAR(28) DEFAULT NULL,
---                           type VARCHAR(21) DEFAULT NULL,
---                           description VARCHAR(180) DEFAULT NULL
---   PRIMARY KEY (id),
---   UNIQUE KEY UX_product_code (code)
--- )
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+    id serial PRIMARY KEY NOT NULL,
+    studentmatric VARCHAR(9) UNIQUE NOT NULL ,
+    studentname VARCHAR(90) NOT NULL
+);
 
 -- MySQL Schema
 
-DROP TABLE IF EXISTS products;
+-- DROP TABLE IF EXISTS students;
 
-CREATE TABLE products (
-                          id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-                          code VARCHAR(9) NOT NULL,
-                          name VARCHAR(90) NOT NULL,
-                          category VARCHAR(28) NOT NULL,
-                          brand VARCHAR(28) DEFAULT NULL,
-                          type VARCHAR(21) DEFAULT NULL,
-                          description VARCHAR(180) DEFAULT NULL,
-                          PRIMARY KEY (id),
-                          UNIQUE KEY UX_product_code (code)
-);
+-- CREATE TABLE students (
+--                           id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+--                           studentmatric VARCHAR(9) NOT NULL,
+--                           studentname VARCHAR(90) NOT NULL,
+--                           PRIMARY KEY (id),
+--                           UNIQUE KEY UX_student_matric (studentmatric)
+-- );
